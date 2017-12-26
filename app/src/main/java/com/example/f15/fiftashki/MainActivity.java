51f0bbc42c74e842bc0490f43edf88478dddedcf
rybin <forknispoon@gmail.com>
2017-12-27 00:24:18 +0300

    start custom image support

--- app/src/main/java/com/example/f15/fiftashki/MainActivity.java
+++ app/src/main/java/com/example/f15/fiftashki/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.f15.fiftashki;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String DEBUG_TAG = "DEBUG";
+//    private static final int IMAGEREQUESTCODE = 8242008;
+    private static final int IMAGEREQUESTCODE = 123;
     public GameLogic game;
 
     @Override
@@ -78,7 +86,11 @@ public class MainActivity extends AppCompatActivity {
 
             for (int i = 0; i < game.getFieldY(); i++) {
                 for (int j = 0; j < game.getFieldX(); j++) {
-                    cr[i*game.getFieldX()+j] = Bitmap.createBitmap(bm, sqr * j / game.getFieldX(), sqr * i / game.getFieldY(), sqr / game.getFieldX(), sqr / game.getFieldY());
+                    if (i == game.getFieldY()-1 && j == game.getFieldX()-1) {
+                        cr[0] = Bitmap.createBitmap(bm, sqr * j / game.getFieldX(), sqr * i / game.getFieldY(), sqr / game.getFieldX(), sqr / game.getFieldY());
+                        break;
+                    }
+                    cr[i*game.getFieldX()+j+1] = Bitmap.createBitmap(bm, sqr * j / game.getFieldX(), sqr * i / game.getFieldY(), sqr / game.getFieldX(), sqr / game.getFieldY());
                 }
             }
         }
@@ -128,5 +140,74 @@ public class MainActivity extends AppCompatActivity {
         int y(int i) {
             return i * sqr / game.getFieldY();
         }
+    }
+
+    private void pickImageFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, IMAGEREQUESTCODE);
+    }
+
+    @Override
+    protected final void onActivityResult(final int requestCode, final int resultCode, final Intent i) {
+        super.onActivityResult(requestCode, resultCode, i);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case IMAGEREQUESTCODE:
+//                    manageImageFromUri(i.getData());
+                    Toast.makeText(getBaseContext(), "Image_get", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        } else {
+            // manage result not ok !
+        }
+
+    }
+
+    private void manageImageFromUri(Uri imageUri) {
+        Bitmap bitmap = null;
+
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        } catch (Exception e) {
+            // Manage exception ...
+        }
+
+        if (bitmap != null) {
+            // Here you can use bitmap in your application ...
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            Toast.makeText(getBaseContext(), "Action_Settings", Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
+
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(getBaseContext(), "Action_Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.getimage:
+                pickImageFromGallery();
+                break;
+            default: break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
